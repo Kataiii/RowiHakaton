7a122942fdcd520c2bb7be5eb8e35e3b9c70797c
katya <katya_yuneva@mail.ru>
2023-05-23 12:32:35 +0400

    routing update

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { RouterProvider } from 'react-router-dom';
+import router from './routing/Router';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
+
+root.render(
+  <RouterProvider router={router} />
+);
--- frontend/src/routing/layouts/root.tsx
+++ frontend/src/routing/layouts/root.tsx
@@ -1,7 +1,12 @@
-export default function Root(){
+interface RoleGuardProps{
+    children : React.ReactElement;
+}
+
+
+const Root = () => {
     return(
-        <div>
-            
-        </div>
+        <div></div>
     )
 }
+
+export default Root;
