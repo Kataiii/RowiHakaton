224dbce3af29615e68495054d7407c9e4b3a4851
katya <katya_yuneva@mail.ru>
2023-05-23 15:20:49 +0400

    update form login, add validate

--- frontend/src/components/FormLogin.tsx
+++ frontend/src/components/FormLogin.tsx
@@ -1,24 +1,49 @@
 import styles from './css/FormLogin.module.css';
-import { Input } from 'antd';
+import { Button, Form, Input } from 'antd';
 import PrimaryButton from './PrimaryButton';
-import { Link } from 'react-router-dom';
-
 
 const FormLogin = () => {
-    return(
-        <div className={styles.FormDiv}>
-            <h1 className={styles.FormTitle}>Авторизация</h1>
-            <div className={styles.FormWrapDiv}>
-                <Input className={styles.FormInput} placeholder="Email" />
-                <Input className={styles.FormInput} placeholder="Пароль" />
-                <div className={styles.FormDivLink}>
-                    <a className={styles.FormLink} href='#'>Забыли пароль?</a>
+
+    const onFinish = (values: any) => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = (errorInfo: any) => {
+        console.log('Failed:', errorInfo);
+    };
+
+    return (
+        <Form
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off">
+            <div className={styles.FormDiv}>
+                <h1 className={styles.FormTitle}>Авторизация</h1>
+                <div className={styles.FormWrapDiv}>
+                <Form.Item
+                name="useremail"
+                rules={[{ required: true, message: 'Введите email' },
+                        { type: 'email', message: 'Неверный формат email'}]}
+                >
+                    <Input className={styles.FormInput} placeholder="Email" />
+                </Form.Item>
+                <Form.Item
+                    name="password"
+                    rules={[{ required: true, message: 'Введите пароль' }]}
+                >
+                    <Input.Password className={styles.FormInput} placeholder="Пароль" />
+                    </Form.Item>
+                    <div className={styles.FormDivLink}>
+                        <a className={styles.FormLink} href='#'>Забыли пароль?</a>
+                    </div>
+                </div>
+                <div className={styles.FormButtonWrap}>
+                <Form.Item>
+                    <PrimaryButton action={() => console.log('Hello world')} content='Войти' />
+                </Form.Item>
                 </div>
             </div>
-            <div className={styles.FormButtonWrap}>
-                <PrimaryButton action={() => console.log('Hello world')} content='Войти'/>
-            </div>
-        </div>
+        </Form>
     )
 }
 
