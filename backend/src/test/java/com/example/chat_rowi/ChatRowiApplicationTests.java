450b6e933c49748c84114098dcea258f9773d9a9
morebeautifulthandoriangray <53139652+morebeautifulthandoriangray@users.noreply.github.com>
2023-05-24 03:31:57 +0400

    Add files via upload

---
+++ backend/src/test/java/com/example/chat_rowi/ChatRowiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chat_rowi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatRowiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
